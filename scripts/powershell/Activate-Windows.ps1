c23f31ee6bf31431d92b934de0f1d3fe85ba086b
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-12-10 14:46:14 -0800

    Update MAS to version 1.7

--- scripts/powershell/Activate-Windows.ps1
+++ scripts/powershell/Activate-Windows.ps1
@@ -1,21 +1,21 @@
-##################
-# Activate Windows
-##################
+# activate Windows using MAS 1.7 (Microsoft Activation Scripts)
+# author: Yuzhou "Joe" Mo (@yuzhoumo)
+# license: GNU GPLv3
 
-# Force run as administrator
+# force run as administrator
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {  
     $arguments = "& '" +$myinvocation.mycommand.definition + "'"
     Start-Process powershell -Verb runAs -ArgumentList $arguments
     Break;
 }
 
-# Windows 11 Enterprise KMS Key
+# Windows 11 Enterprise KMS key
 $enterpriseKey = "NPPR9-FWDCX-D2C8J-H872K-2YT43"
 $scriptDir = Split-Path $MyInvocation.MyCommand.Path
-$activator = "$scriptDir\..\..\assets\HWID_Activation.cmd"
+$activator = "$scriptDir\..\..\assets\mas_aio.cmd"
 
 Write-Host "> Setting KMS Key for Windows 11 Enterprise"
 cscript C:\Windows\System32\slmgr.vbs /ipk $enterpriseKey
 
 Write-Host "> Activating Windows"
-Start-Process $activator -ArgumentList "/u" -wait
+Start-Process $activator -ArgumentList "/HWID" -Wait
